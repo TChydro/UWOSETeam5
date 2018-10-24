e36d052c5614f460c5f3bb726830e9a4743be9a9
richts22 <richts22@UWO5336W0>
2018-10-24 14:35:51 -0500

    Comment

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 100;
+            for (int i = 1; i <= number; i++)
+            {
+                if (i % 3 == 0)
+                    Console.WriteLine("Fizz");
+                else if (i % 5 == 0)
+                    Console.WriteLine("Buzz");
+                else if (i % 15 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                
+            }
+        }
+    }
+}
