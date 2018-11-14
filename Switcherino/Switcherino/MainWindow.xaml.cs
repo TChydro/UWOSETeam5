3e3ca151adde90ff6c90f38d8aec6c06f814bc82
TChydro <33527113+TChydro@users.noreply.github.com>
2018-11-14 14:42:55 -0600

    Bare-bones class for swapping windows
    Use this as a base for adding the other gui elements and functionality. All the files are set up, they just need the actual code added to them.

---
+++ Switcherino/Switcherino/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Switcherino
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SwapSignIn(object sender, RoutedEventArgs e)
+        {
+            Window signInWindow = new SignIn();
+            signInWindow.Show();
+            this.Close();
+        }
+
+        private void SwapPhoneEmail(object sender, RoutedEventArgs e)
+        {
+            Window phoneEmailWindow = new PhoneEmail();
+            phoneEmailWindow.Show();
+            this.Close();
+        }
+
+        private void SwapDownloadData(object sender, RoutedEventArgs e)
+        {
+            Window downloadDataWindow = new DownloadData();
+            downloadDataWindow.Show();
+            this.Close();
+        }
+    }
+}
---
+++ Switcherino/Switcherino/PhoneEmail.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Switcherino
+{
+    /// <summary>
+    /// Interaction logic for PhoneEmail.xaml
+    /// </summary>
+    public partial class PhoneEmail : Window
+    {
+        public PhoneEmail()
+        {
+            InitializeComponent();
+        }
+    }
+}
